b55fac3076d7728bb4cef2c52b2b9b4392317d79
Elpah <obengelpachris@gmail.com>
2025-09-27 08:32:49 +0200

    save progress

---
+++ cpp 01/ex00/Zombie.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elpah <elpah@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 02:03:39 by elpah             #+#    #+#             */
+/*   Updated: 2025/09/22 02:05:25 by elpah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <string>
+
+class Zombie
+{
+private:
+	std::string name;
+public:
+	Zombie(/* args */);
+	~Zombie();
+};
+
+Zombie::Zombie(/* args */)
+{
+}
+
+Zombie::~Zombie()
+{
+}
+
+
+#endif
