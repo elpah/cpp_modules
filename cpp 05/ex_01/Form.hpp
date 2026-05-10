c871d5a91172b9f0c10c8b3758d65c702167e860
Elpah <obengelpachris@gmail.com>
2026-05-10 05:52:17 +0200

    added files from ex01 and created Form class

---
+++ cpp 05/ex_01/Form.hpp
@@ -0,0 +1,44 @@
+
+
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat;
+
+class Form
+{
+private:
+	const std::string _name;
+	const int _gradeToSign;
+	const int _gradeToExecute;
+	bool _isSigned;
+
+public:
+	Form();
+	Form(const std::string &name, int gradeToSign, int gradeToExecute);
+	Form(const Form &other);
+	Form &operator=(const Form &other);
+	bool getIsSigned() const;
+	const std::string &getName(void) const;
+	int getGradeToSign() const;
+	int getGradeToExecute() const;
+	void beSigned(const Bureaucrat &bureaucrat);
+	~Form();
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		const char *what() const throw();
+	};
+};
+std::ostream &operator<<(std::ostream &out, const Form &input);
+
+#endif
