4a34b76d404406d30556fc82d31511c9c958e376
Elpah <obengelpachris@gmail.com>
2026-05-13 05:08:20 +0200

    Added exeecuteForm function declaration and implementation

---
+++ cpp 05/ex_02/Bureaucrat.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elpah <elpah@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/04 01:23:49 by elpah             #+#    #+#             */
+/*   Updated: 2026/05/13 05:05:58 by elpah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class AForm;
+
+class Bureaucrat
+{
+private:
+	const std::string _name;
+	int _grade;
+
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string &name, int grade);
+	Bureaucrat(const Bureaucrat &other);
+	Bureaucrat &operator=(const Bureaucrat &other);
+	const std::string &getName(void) const;
+	int getGrade(void) const;
+	void incrementGrade();
+	void decrementGrade();
+	void signForm(AForm &form) const;
+	void executeForm(AForm const &form) const;
+	~Bureaucrat();
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		const char *what() const throw();
+	};
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &input);
+
+#endif
