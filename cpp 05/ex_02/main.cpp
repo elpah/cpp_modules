1206aec52048039d9fa08d5c84c6fa17828f1fe7
Elpah <obengelpachris@gmail.com>
2026-05-17 04:31:34 +0200

    created some tests in main

--- cpp 05/ex_02/main.cpp
+++ cpp 05/ex_02/main.cpp
@@ -1,30 +1,35 @@
+
+#include <ctime>
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main(void)
 {
-	// try
-	// {
-	// 	Bureaucrat Elpah("Elpah", 49);
-	// 	Form form1("Tax Form", 140, 100);
-	// 	Form form2("Secret Form", 50, 25);
+	try
+	{
+		std::srand(std::time(0));
+		Bureaucrat boss("Boss", 1);
+		Bureaucrat manager("ceo", 10);
+		Bureaucrat employee("employee", 150);
 
-	// 	std::cout << Elpah << std::endl;
-	// 	std::cout << form1 << std::endl;
-	// 	std::cout << form2 << std::endl;
+		ShrubberyCreationForm shrub("home");
+		RobotomyRequestForm robot("Bender");
+		PresidentialPardonForm pardon("Arthur Dent");
+		boss.signForm(shrub);
+		boss.executeForm(shrub);
+		boss.signForm(robot);
+		boss.signForm(pardon);
 
-	// 	Elpah.signForm(form1);
-	// 	Elpah.signForm(form2);
-
-	// 	Elpah.incrementGrade();
-
-	// 	std::cout << Elpah << std::endl;
-
-	// 	Elpah.signForm(form1);
-	// }
-	// catch (const std::exception &e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-	// return 0;
+		manager.executeForm(shrub);
+		manager.executeForm(robot);
+		manager.executeForm(pardon);
+		employee.executeForm(shrub);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 }
