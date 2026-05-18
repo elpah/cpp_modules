26e5d26840847bbfbdc45d9b7292082d7bba16d3
Elpah <obengelpachris@gmail.com>
2026-05-18 06:04:15 +0200

    implemented some class member functions and some helper functions'

---
+++ cpp 06/ScalarConverter.cpp
@@ -0,0 +1,59 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &other)
+{
+	(void)other;
+}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other)
+{
+	(void)other;
+	return (*this);
+}
+
+ScalarConverter::~ScalarConverter() {}
+
+bool checkIsPseudoLiteral(const std::string &input)
+{
+	if (input == "-inf" || input == "+inf" || input == "nan" || input == "-inff" || input == "+inff" || input == "nanf")
+		return (true);
+	return (false);
+}
+
+bool checkInput(const std::string &input)
+{
+
+	size_t i = 0;
+	bool isDecimal = false;
+	bool hasDigit = false;
+
+	if (checkIsPseudoLiteral(input))
+		return (true);
+	if (input.empty())
+		return false;
+	if (input[i] == '-')
+		i++;
+	for (i; i < input.size(); i++)
+	{
+		if (isdigit(input[i]))
+			hasDigit = true;
+		else if (input[i] == '.' && !isDecimal)
+			isDecimal = true;
+		else if (input[i] == 'f' && i == input.size() - 1)
+			continue;
+		else
+			return (false);
+	}
+	return (hasDigit);
+}
+
+bool CheckIsInt(const std::string &input)
+{
+	if (checkIsPseudoLiteral(input))
+		return (false);
+	if (atof(input.c_str()) - atoi(input.c_str()) != 0)
+		return (false);
+	return (true);
+}
