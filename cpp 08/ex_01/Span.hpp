8a73f53faf78135089407406f13a0ac772527395
Elpah <obengelpachris@gmail.com>
2026-05-31 03:41:00 +0200

    add number using iterator range

--- cpp 08/ex_01/Span.hpp
+++ cpp 08/ex_01/Span.hpp
@@ -3,23 +3,36 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <stdexcept>
 
 class Span
 {
 private:
 	std::vector<int> _storage;
-	int _max_element;
+	unsigned int _max_element;
 
 public:
 	Span();
-	Span(int N);
+	Span(unsigned int N);
 	Span(const Span &other);
 	Span &operator=(const Span &other);
 	void addNumber(int to_add);
 	int shortestSpan();
 	int longestSpan();
-	
+	template <typename Iterator>
+	void addNumbers(Iterator begin, Iterator end);
+
 	~Span();
 };
 
+template <typename Iterator>
+void Span::addNumbers(Iterator begin, Iterator end)
+{
+	if (_storage.size() + std::distance(begin, end) > _max_element)
+		throw std::out_of_range("Storage is already full");
+
+	_storage.insert(_storage.end(), begin, end);
+}
 #endif
